115b7565b4beebd069019a71e0bd7ebb80722b8b
Sindhura Chinthakindi <sindhurachinthakindi@gmail.com>
2026-05-07 13:28:23 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-kupaf5&438b%^yxp3u@z_5@-t0v116zw$!y^m@lhs9)=4)lwh-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 LOGIN_URL='/login'
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/login/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATIC_URL = 'static/'
